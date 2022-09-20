5b32988ddefd4967a13fb906c43882db0e070d07
KillerAnime666 <killeranime666@gmail.com>
2022-09-20 19:28:00 -0400

    Incorporada la carpeta de recursos y la imagen principal

--- index.php
+++ index.php
@@ -3,10 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
+    <header>
+        <nav class="navbar navbar-expand" style="background-color: #CBE0F2;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">
+                  <img src="resources/images/icon.png" alt="GameStore">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="catalogo.php">Muebles a la Venta</a>
+                    <a class="nav-link" href="login.php">Pedidos</a>
+                    <a class="nav-link" href="contactanos.php">Contáctanos</a>
+                </div>
+            </div>
+        </nav>
+    </header>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
