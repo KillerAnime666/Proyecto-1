76c94b2f56bc6dcaa0bf856cf26a871608bcd854
KillerAnime666 <killeranime666@gmail.com>
2022-09-20 20:46:38 -0400

    Implementando el login/register con css y Js

--- index.php
+++ index.php
@@ -1,29 +1,56 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="resources/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
-<body>
-    <header>
-        <nav class="navbar navbar-expand" style="background-color: #CBE0F2;">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">
-                  <img src="resources/images/icon.png" alt="GameStore">
+<body>/
+  <!--   <header>
+        <ul class="nav justify-content-center" style="background-color: #CBE0F2;">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">
+                    <img src="resources/images/icon.png" alt="GameStore">
                 </a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="catalogo.php">Muebles a la Venta</a>
-                    <a class="nav-link" href="login.php">Pedidos</a>
-                    <a class="nav-link" href="contactanos.php">Contáctanos</a>
+            </li>
+        </ul>
+    </header> -->
+    <section>
+        <main>
+            <div class="contenedor__todo">
+                <div class="caja__trasera">
+                    <div class="caja__trasera-login">
+                        <h3>¿Ya tienes una cuenta?</h3>
+                        <p>Inicia sesion para entrar a la página</p>
+                        <button id="btn__iniciar-sesion">Iniciar Sesion</button>
+                    </div>
+                    <div class="caja__trasera-register">
+                        <h3>¿Aún no tienes una cuenta?</h3>
+                        <p>Regístrate para que puedas iniciar sesion</p>
+                        <button id="btn__registrarse">registrarse</button>
+                    </div>
+                </div>
+                <div class="contenedor__login-register">
+                    <form action="" class="formulario__login">
+                        <h2>Iniciar Sesion</h2>
+                        <input type="text" placeholder="Usuario">
+                        <input type="password" placeholder="Contraseña">
+                        <button>Entrar</button>
+                    </form>
+                    <form action="" class="formulario__register">
+                        <h2>Regístrate</h2>
+                        <input type="text" placeholder="Nombre Completo">
+                        <input type="text" placeholder="Correo Electronico">
+                        <input type="text" placeholder="Usuario">
+                        <input type="password" placeholder="Contraseña">
+                        <button>Registrarse</button>
+                    </form>
                 </div>
             </div>
-        </nav>
-    </header>
+        </main>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
